eb46f54aaf4405e57da0da6165c3820b7046aa84
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-12-03 13:33:39 -0300

    Prepara buffer e pilha

--- parser.py
+++ parser.py
@@ -1,7 +1,20 @@
-parsing_table = {
+PARSING_TABLE = {
     ('S', 'a'): ['a', 'S', 'b'],
     ('S', 'b'): [],
     ('S', '$'): []
 }
 
 w = "aabb"
+
+def parser(w, parsingTable):
+
+  # Prepara buffer com string de texto + $ ao final
+  buffer = list(w)
+  buffer.append("$")
+
+  #prepara a pilha com S$
+  stack = ['$'] + ["S"]
+
+  print(buffer)
+
+parser(w, PARSING_TABLE)
